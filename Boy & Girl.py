67ae7818f3b5b6493ee5424d97401782f9e7821b
Saeideh-Moghaddam <108235776+saeideh-moghaddam@users.noreply.github.com>
2022-06-29 23:38:58 +0430

    Add files via upload

---
+++ Boy & Girl.py
@@ -0,0 +1,13 @@
+import random 
+
+girl = ['sara', 'zari', 'neda', 'homa', 'eli', 'goli', 'mary', 'mina']
+boy = ['ali', 'reza', 'yasin', 'benyamin', 'mehrdad', 'sajjad', 'aidin', 'shahin']
+
+marrid1= random.shuffle(girl) and (boy)  
+
+for i in range(len(girl)):
+    
+     j=(girl[i],boy[i])        
+#print(girl ,"&" ,boy)
+
+     print(j)
